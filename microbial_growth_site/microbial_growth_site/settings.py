2873d1290f7c50f7f07c94d0a148829bcdf5aae7
ptonner <peter.tonner@gmail.com>
2015-12-23 13:20:29 -0500

    update host

--- microbial_growth_site/microbial_growth_site/settings.py
+++ microbial_growth_site/microbial_growth_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '60vy2@4rxe8-1k&=4et*s%ja8aq)1t($e(lfeboh!oct%e8n*!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
